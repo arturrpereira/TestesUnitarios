0a22ae978f4b9ec3d7026c89fb324b2e2947f351
arturrpereira <artur.fernando@protonmail.com>
2023-10-19 22:01:52 -0300

    Test file organization

--- src/main/java/br/arturfernando/servicos/LocacaoService.java
+++ src/main/java/br/arturfernando/servicos/LocacaoService.java
@@ -5,13 +5,21 @@ import static br.arturfernando.utils.DataUtils.adicionarDias;
 import java.io.File;
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.arturfernando.entidades.Filme;
 import br.arturfernando.entidades.Locacao;
 import br.arturfernando.entidades.Usuario;
 import br.arturfernando.utils.DataUtils;
 
 public class LocacaoService {
-	
+
+	public String vPublica;
+	protected String vProtegida;
+	private String vPrivada;
+	String vDefault;
+
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
@@ -30,18 +38,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-
-		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-
-		//verificacao
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-	}
+	
 }
---
+++ src/test/java/br/arturfernando/servicos/LocacaoServiceTest.java
@@ -0,0 +1,31 @@
+package br.arturfernando.servicos;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.arturfernando.entidades.Filme;
+import br.arturfernando.entidades.Locacao;
+import br.arturfernando.entidades.Usuario;
+import br.arturfernando.utils.DataUtils;
+
+public class LocacaoServiceTest {
+
+    @Test
+    public void teste() {
+        // cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        // acao
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        // verificacao
+        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+
+}
